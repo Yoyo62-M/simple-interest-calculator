fad49a27e14bfb58b04defe57ef054eed801eda9
Yoyo62-M <sculffissmbah@gmail.com>
2026-04-19 17:19:31 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# This script calculates simple interest given principal, rate of interest and time.
-# Simple Interest = Principal x Rate x Time / 100
+# Simple Interest Calculator
 
-echo "Enter the principal amount:"
+echo "Enter Principal amount:"
 read principal
 
-echo "Enter the rate of interest (in %):"
+echo "Enter Rate of Interest (%):"
 read rate
 
-echo "Enter the time (in years):"
+echo "Enter Time Period (years):"
 read time
 
-simple_interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+
 echo "Simple Interest = $simple_interest"
